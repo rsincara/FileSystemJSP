dfd0eb94c6477cb63bfd7eda1faf39d75427b4cf
Maksim <rsincara@mail.ru>
2021-09-19 17:27:32 +0500

    Working FileSystem

---
+++ src/main/java/Models/MyFile.java
@@ -0,0 +1,36 @@
+package Models;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class MyFile {
+    public String fileName;
+    public String absolutePath;
+    public String creationDate;
+    public long size; //в байтах
+
+    public MyFile(File file) throws IOException {
+        this.fileName = file.getName();
+        this.absolutePath = file.getAbsolutePath();
+        this.creationDate = Files.readAttributes(file.toPath(), BasicFileAttributes.class).creationTime().toString().substring(0,10);
+        this.size = file.length();
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getAbsolutePath(){
+        return absolutePath;
+    }
+
+    public String getCreationDate(){
+        return creationDate;
+    }
+
+    public String getSize(){
+        return String.valueOf(size);
+    }
+}
